7cfe3d5e89901c4805d820bfa60483bc8cf293b2
williambilsdon <williambilsdon@gmail.com>
2023-07-01 09:01:45 +0100

    feat : using arboard to get text from clipboard on paste

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,39 @@
 use std::io::{stdout, Write};
-
-use crossterm::{event::{Event, KeyCode, read}, terminal, execute, cursor::MoveTo, Result};
+use arboard::Clipboard;
+use crossterm::{event::{Event, KeyCode, read, KeyEvent, KeyModifiers}, terminal, execute, cursor::MoveTo, Result};
 
 fn main() -> Result<()>{
 
     execute!(stdout(), terminal::EnterAlternateScreen)?;
+    execute!(stdout(), crossterm::cursor::Hide)?;
 
     let mut buffer = String::new();
-
-    execute!(stdout(), crossterm::cursor::Hide)?;
+    let mut clipboard = Clipboard::new().expect("error setting up clipboard");
 
     loop {
         execute!(stdout(), terminal::Clear(terminal::ClearType::All))?;
         execute!(stdout(), MoveTo(0, 0), crossterm::style::Print(&buffer))?;
         execute!(stdout(), MoveTo(buffer.len() as u16, 0))?;
-
         stdout().flush()?;
        
         if let Event::Key(event) = read()? {
-            match event.code {
-                KeyCode::Char(c) => {
+            match event {
+                KeyEvent{code: KeyCode::Char('c'), modifiers: KeyModifiers::CONTROL, ..} => {
+                    buffer.push_str(clipboard.get_text()
+                                .expect("Error getting clipboard text")
+                                .as_str()
+                            )
+                },
+                KeyEvent{code: KeyCode::Char(c), ..} => {
                     buffer.push(c);
                 },
-                KeyCode::Backspace => {
+                KeyEvent{code: KeyCode::Backspace, ..} => {
                     buffer.pop();
                 },
-                KeyCode::Esc => break,
+                KeyEvent{code: KeyCode::Esc, .. } => break,
                 _ => {}
             }
         } 
-
     }
 
     execute!(stdout(), crossterm::cursor::Show)?;
