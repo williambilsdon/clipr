324be6b754a37503858c8d37e540a6c66a68721a
williambilsdon <williambilsdon@gmail.com>
2023-07-03 17:20:57 +0100

    fix : enabled raw mode to fix exit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::io::{stdout, Write};
 use arboard::Clipboard;
-use crossterm::{event::{Event, KeyCode, read, KeyEvent, KeyModifiers}, terminal, execute, cursor::MoveTo, Result};
+use crossterm::{event::{Event, KeyCode, read, KeyEvent, KeyModifiers}, terminal::{self, enable_raw_mode, disable_raw_mode}, execute, cursor::MoveTo, Result};
 
 fn main() -> Result<()>{
 
     execute!(stdout(), terminal::EnterAlternateScreen)?;
     execute!(stdout(), crossterm::cursor::Hide)?;
+    enable_raw_mode()?;
 
     let mut buffer = String::new();
     let mut clipboard = Clipboard::new().expect("error setting up clipboard");
@@ -18,11 +19,14 @@ fn main() -> Result<()>{
        
         if let Event::Key(event) = read()? {
             match event {
-                KeyEvent{code: KeyCode::Char('c'), modifiers: KeyModifiers::CONTROL, ..} => {
+                KeyEvent{code: KeyCode::Char('v'), modifiers: KeyModifiers::CONTROL, ..} => {
                     buffer.push_str(clipboard.get_text()
                                 .expect("Error getting clipboard text")
                                 .as_str()
                             )
+                },
+                KeyEvent{code: KeyCode::Char('s'), modifiers: KeyModifiers::CONTROL, ..} => {
+                    buffer.push_str("Saving file...")
                 },
                 KeyEvent{code: KeyCode::Char(c), ..} => {
                     buffer.push(c);
@@ -36,8 +40,10 @@ fn main() -> Result<()>{
         } 
     }
 
+    disable_raw_mode()?;
     execute!(stdout(), crossterm::cursor::Show)?;
     execute!(stdout(), terminal::LeaveAlternateScreen)?;
+    
     Ok(())
 }
 
