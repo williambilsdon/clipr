67b372f1b49951a0ba7b7629b613f3f640f1bbce
williambilsdon <williambilsdon@gmail.com>
2023-07-05 07:40:03 +0100

    feat : add file write and simplify key inputs

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,59 @@
-use std::io::{stdout, Write};
-use arboard::Clipboard;
-use crossterm::{event::{Event, KeyCode, read, KeyEvent, KeyModifiers}, terminal::{self, enable_raw_mode, disable_raw_mode}, execute, cursor::MoveTo, Result};
+use std::{io::{stdout, Write}, path::Path, fs};
+use crossterm::{event::{Event, KeyCode, read, KeyEvent}, terminal::{self, enable_raw_mode, disable_raw_mode}, execute, cursor::MoveTo, Result};
+
+const ROOT_ADDR: &str = "/home/wbilsdon/Documents/clipr/";
+
+struct File {
+    content: String,
+    name: String
+}
+
+impl File {
+    pub fn new() -> File {
+        File {
+            content: String::new(),
+            name: String::new()
+        }
+    }
+}
 
 fn main() -> Result<()>{
+    
+    let mut file = File::new();
+
+    println!("Name the new file :");
+    let _ = std::io::stdin().read_line(&mut file.name).unwrap();
 
     execute!(stdout(), terminal::EnterAlternateScreen)?;
     execute!(stdout(), crossterm::cursor::Hide)?;
     enable_raw_mode()?;
 
-    let mut buffer = String::new();
-    let mut clipboard = Clipboard::new().expect("error setting up clipboard");
-
     loop {
         execute!(stdout(), terminal::Clear(terminal::ClearType::All))?;
-        execute!(stdout(), MoveTo(0, 0), crossterm::style::Print(&buffer))?;
-        execute!(stdout(), MoveTo(buffer.len() as u16, 0))?;
+        execute!(stdout(), MoveTo(0, 0), crossterm::style::Print(&file.content))?;
+        execute!(stdout(), MoveTo(file.content.len() as u16, 0))?;
         stdout().flush()?;
        
         if let Event::Key(event) = read()? {
             match event {
-                KeyEvent{code: KeyCode::Char('v'), modifiers: KeyModifiers::CONTROL, ..} => {
-                    buffer.push_str(clipboard.get_text()
-                                .expect("Error getting clipboard text")
-                                .as_str()
-                            )
-                },
-                KeyEvent{code: KeyCode::Char('s'), modifiers: KeyModifiers::CONTROL, ..} => {
-                    buffer.push_str("Saving file...")
-                },
                 KeyEvent{code: KeyCode::Char(c), ..} => {
-                    buffer.push(c);
+                    file.content.push(c);
+                },
+                KeyEvent{code: KeyCode::Enter, ..} => {
+                    file.content.push('\n');
                 },
                 KeyEvent{code: KeyCode::Backspace, ..} => {
-                    buffer.pop();
+                    file.content.pop();
                 },
-                KeyEvent{code: KeyCode::Esc, .. } => break,
+                KeyEvent{code: KeyCode::Esc, .. } => {
+                    let string_path = format!("{}{}", ROOT_ADDR, file.name.as_str());
+                    let path = Path::new(string_path.trim());
+
+                    let mut output = fs::File::create(path)?;
+                    output.write_all(file.content.as_bytes())?;
+
+                    break
+                },
                 _ => {}
             }
         } 
@@ -43,7 +62,7 @@ fn main() -> Result<()>{
     disable_raw_mode()?;
     execute!(stdout(), crossterm::cursor::Show)?;
     execute!(stdout(), terminal::LeaveAlternateScreen)?;
-    
+
     Ok(())
 }
 
