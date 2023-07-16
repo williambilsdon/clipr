232996dd29f1c060a84cd341fc928af0ee8a392b
williambilsdon <williambilsdon@gmail.com>
2023-07-16 14:50:57 +0100

    wip : use list widget for files

--- src/ui/list.rs
+++ src/ui/list.rs
@@ -1,20 +1,17 @@
 use std::{io::Stdout, error::Error, fs};
 use crossterm::event::{read, Event, KeyCode};
-use ratatui::{Terminal, backend::CrosstermBackend, widgets::Paragraph};
+use ratatui::{Terminal, backend::CrosstermBackend, widgets::{List, ListItem}, text::{Line, Span}};
 use crate::ui::model::ROOT_ADDR;
 
 pub fn list_mode(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn Error>> {
     loop {
         let files = fs::read_dir(ROOT_ADDR)?;
-        let mut file_list = String::new();
-        for file in files.into_iter() {
-            let name = format!("{}\n", file.unwrap().file_name().to_str().unwrap());
-            file_list.push_str(name.as_str());
-            
-        };
+        let file_list: Vec<ListItem> = files.map(|res| res.map(|f| {
+            ListItem::new(Line::from(Span::raw(format!("{}", f.file_name().to_str().unwrap()))))
+        }).unwrap()).collect();
         
         terminal.draw(|frame| {
-            let name_file = Paragraph::new(file_list.as_str());
+            let name_file = List::new(file_list);
             frame.render_widget(name_file, frame.size());
         })?;
 
